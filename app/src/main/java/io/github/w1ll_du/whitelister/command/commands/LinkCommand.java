d3d11aa905cebd26aed6a7195326b23f71b70ae1
William Du <duwill1412@gmail.com>
2022-01-14 21:12:01 -0500

    something

--- app/src/main/java/io/github/w1ll_du/whitelister/command/commands/LinkCommand.java
+++ app/src/main/java/io/github/w1ll_du/whitelister/command/commands/LinkCommand.java
@@ -16,50 +16,49 @@ public class LinkCommand implements ICommand {
             new UnlinkCommand().handle(ctx);
             return;
         }
-        String username = ctx.getArgs().get(0);
-        if (ctx.getPlayerMap().containsKey(ctx.getAuthor().getId())) {
-            new UnlinkCommand().handle(ctx);
-        }
-        if (! isValidUsername(username)) {
+        String name = ctx.getArgs().get(0);
+        if (! isValidUsername(name)) {
             ctx.getChannel().sendMessage("Username invalid").queue();
             return;
         }
-        if (ctx.getPlayerMap().containsValue(username)) {
-            ctx.getChannel().sendMessage(username
-                    + " is already linked with "
-                    + ctx.getGuild().getMemberById(ctx.getPlayerMap().inverseBidiMap().get(username)).getEffectiveName()
-                    + ".")
+        // already linked with another disc account
+        if (ctx.getPlayerMap().containsValue(name)) {
+            ctx.getChannel().sendMessage(name + " is already linked.")
                 .queue();
+            return;
+        }
+        // already linked with another mc account
+        if (ctx.getPlayerMap().containsKey(ctx.getAuthor().getId())) {
+            new UnlinkCommand().handle(ctx);
         }
         // https://github.com/SparklingComet/java-mojang-api
-        else {
+        try {
+            // checks validity of name
+            JSONObject resp = Utils.getJSONObject("https://api.mojang.com/users/profiles/minecraft/" + name);
+            // enforce the case
+            String username = (String) resp.get("name");
+            // check case matching
+            if (!username.equals(name)) {
+                ctx.getChannel().sendMessage("The case does not match, please try again.").queue();
+                ctx.getChannel().sendMessage("The proper name is " + username + ".").queue();
+                return;
+            }
+            ctx.getPlayerMap().put(ctx.getAuthor().getId(), username);
+            Utils.rconCommand("whitelist add " + username);
             try {
-                // checks validity of name
-                JSONObject resp = Utils.getJSONObject("https://api.mojang.com/users/profiles/minecraft/" + username);
-                // makes sure the case is right
-                username = (String) resp.get("name");
-                if (ctx.getPlayerMap().containsKey(ctx.getAuthor().getId())) {
-                    String oldName = ctx.getPlayerMap().get(ctx.getAuthor().getId());
-                    Utils.rconCommand("whitelist remove " + oldName);
-                    ctx.getChannel().sendMessage("Unliked with " + oldName).queue();
-                }
-                ctx.getPlayerMap().put(ctx.getAuthor().getId(), username);
-                Utils.rconCommand("whitelist add " + username);
-                try {
-                    ObjectMapper mapper = new ObjectMapper();
-                    mapper.writeValue(Paths.get("playerMap.json").toFile(), ctx.getPlayerMap());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Utils.rconCommand("whitelist reload");
-                ctx.getChannel().sendMessage("Linked with " + username).queue();
-                ctx.getMember().modifyNickname(username).queue();
-                ctx.getGuild().addRoleToMember(ctx.getMember(),
-                        ctx.getGuild().getRoleById(ctx.getConf().get("whitelist_role_id"))).queue();
-            } catch (Exception e) {
-                ctx.getChannel().sendMessage("The account does not exist.").queue();
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.writeValue(Paths.get("playerMap.json").toFile(), ctx.getPlayerMap());
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+            Utils.rconCommand("whitelist reload");
+            ctx.getChannel().sendMessage("Linked with " + username).queue();
+            ctx.getMember().modifyNickname(username).queue();
+            ctx.getGuild().addRoleToMember(ctx.getMember(),
+                    ctx.getGuild().getRoleById(ctx.getConf().get("whitelist_role_id"))).queue();
+        } catch (Exception e) {
+            ctx.getChannel().sendMessage("The account does not exist.").queue();
+            e.printStackTrace();
         }
     }
 
