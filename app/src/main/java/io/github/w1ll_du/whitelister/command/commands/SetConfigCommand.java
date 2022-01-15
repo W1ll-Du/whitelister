0d29a977d9ba113b343a43c39892f69ddf3d0139
William Du <duwill1412@gmail.com>
2022-01-14 21:11:29 -0500

    add

---
+++ app/src/main/java/io/github/w1ll_du/whitelister/command/commands/SetConfigCommand.java
@@ -0,0 +1,17 @@
+package io.github.w1ll_du.whitelister.command.commands;
+
+import io.github.w1ll_du.whitelister.command.AAdminCommand;
+import io.github.w1ll_du.whitelister.command.CommandContext;
+
+public class SetConfigCommand extends AAdminCommand {
+    // TODO
+    @Override
+    protected void handle2(CommandContext ctx) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "setConfig";
+    }
+}
