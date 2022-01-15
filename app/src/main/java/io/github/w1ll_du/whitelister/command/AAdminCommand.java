ec86dbc2619ec4d00699fd8695d977b0af57960d
William Du <duwill1412@gmail.com>
2022-01-14 21:09:49 -0500

    actually enforce admin

--- app/src/main/java/io/github/w1ll_du/whitelister/command/AAdminCommand.java
+++ app/src/main/java/io/github/w1ll_du/whitelister/command/AAdminCommand.java
@@ -5,7 +5,9 @@ public abstract class AAdminCommand implements ICommand {
     @Override
     public void handle(CommandContext ctx) {
         // make sure is owner
-        handle2(ctx);
+        if (ctx.getConf().get("owner_id").equals(ctx.getAuthor().getId())) {
+            handle2(ctx);
+        }
     }
 
     protected abstract void handle2(CommandContext ctx);
